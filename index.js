c04b0e335c35509b95f6e0eb1d4c5c386f0b71d0
yeonuk <pyu7215@gmail.com>
2021-08-06 16:27:59 +0900

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express') //express모듈 가져오기
+const app = express()   //express 함수사용하여 app생성
+const port = 3000
+
+//mongoose를 이용하여 mongoDB 연결
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yeonuk:pyu10812!!@boilerplate.zmh0f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+  useNewUrlParser : true, useUnifiedTopology: true, useCreateIndex : true, useFindAndModify: false
+}).then(() => console.log('mongoDB Connected...'))
+  .catch(err => console.log(err))
+
+//root 디렉토리에 hello world 출력
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
